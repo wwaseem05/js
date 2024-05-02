0d5c2fafe07aec4d01ce5d82938f41e7947c0398
wwaseem05 <wwaseem@academic.rrc.ca>
2024-05-02 10:44:17 -0500

    Updated sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,101 @@
-alert("hello, world");
+// alert("hello, world");
 
 // let is when you might wanna change the value 
-let age = 25;
-let year = 2019;
-console.log(age, year);
+// let age = 25;
+// let year = 2019;
+// console.log(age, year);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
 
 // const is global, value doesnt change
-const points = 100;
-console.log(points)
+// const points = 100;
+// console.log(points)
 
 // Old way is var
-var score = 75;
-console.log(score);
+// var score = 75;
+// console.log(score);
 
-let email = 'some@thing.rrc.ca';
-console.log(email);
+// let email = 'some@thing.rrc.ca';
+// console.log(email);
 
 //concatenations of strings 
-let firstName = 'Brenda';
-let lastName = 'Anderson';
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
+// let firstName = 'Brenda';
+// let lastName = 'Anderson';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
 
 //string lenghts
-console.log(fullName.length);
+//console.log(fullName.length);
 
 //string methods
-console.log(fullName.toUpperCase());
+//console.log(fullName.toUpperCase());
 
 //storing string fucntion in a variable 
-let result = fullName.toLocaleLowerCase();
-console.log(result);
-console.log(result, fullName);
+// let result = fullName.toLocaleLowerCase();
+// console.log(result);
+// console.log(result, fullName);
 
-let index = email.indexOf('@');
-console.log(index);
+//COMMON STRING METHODS
+
+//method to find position of a character 
+// let index = email.indexOf('@');
+// console.log(index);
+
+//slice
+// let result = email.slice(0,4);
+// console.log(result);
+
+//10 is length
+// let result = email.substr(4,12);
+// console.log(result)
+
+// let result = email.replace('m','w');
+// console.log(result)
+
+//      MATH OPERATORS  //
+// L
+
+// Template Strings - Use Backticks `` not ''
+
+// const title = 'Best Reads of The Month'
+// const author = 'Mario'
+// const likes = 30;
+
+// let result = `The blog ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// // Creating HTML templates 
+
+// let html = `
+// <h2>${title}</h2>
+// <p>${author}</p>
+// <span>This blog has ${likes} likes</span>
+// `;
+
+// console.log(html);
+
+//          ARRAYS          //
+// L
+//          UNDEFINED       //
+
+// let age;
+// console.log(age, age+3, `the age is ${age}`);
+
+//          BOOLEANS        //
+
+//          TYPE CONVERSION //
+
+// let score = 100;
+
+// // score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+//           LOOPS          //
+
+for(let i = 0; i <5; i ++){
 
 
-
+    
+}
